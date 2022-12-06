16f0042da93d1f2eb351bbf3671e0511bc5e3309
Irving Molina <dev.irvmol@gmail.com>
2022-12-05 21:25:50 -0700

    added serialize function to contact.js to handle form data

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -1,22 +1,36 @@
-$(document).ready(function () {
-    $("form").submit(function (event) {
-      var formData = {
-        name: $("#name").val(),
-        email: $("#email").val(),
-        subject: $("#pet").val(),
-        message: $("#message").val()
-      };
+// $(document).ready(function () {
+//     $("form").submit(function (event) {
+//       var formData = {
+//         name: $("#name").val(),
+//         email: $("#email").val(),
+//         subject: $("#pet").val(),
+//         message: $("#message").val()
+//       };
   
-      $.ajax({
-        type: "POST",
-        url: "assets/php/contact.php",
-        data: formData,
-        dataType: "json",
-        encode: true,
-      }).done(function (data) {
-        console.log(data);
-      });
+//       $.ajax({
+//         type: "POST",
+//         url: "assets/php/contact.php",
+//         data: formData,
+//         dataType: "json",
+//         encode: true,
+//       }).done(function (data) {
+//         console.log(data);
+//       });
   
+//       event.preventDefault();
+//     });
+//   });
+
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    
       event.preventDefault();
-    });
+      var formValues= $(this).serialize();
+
+      $.post("assets/php/contact.php", formValues, function(data){
+          // Display the returned data in browser
+          console.log(data);
+      });
   });
+});
